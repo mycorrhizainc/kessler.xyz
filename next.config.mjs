78b619f83efb7e653cae0304ee5b133bda204caa
M B <mirrir@mirrir.net>
2024-05-05 18:17:10 -0600

    enable Static Site Rendering for 'npm run build'

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  distDir: 'build',
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
